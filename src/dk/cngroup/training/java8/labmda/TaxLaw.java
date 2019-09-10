8725e0e2a51162f26bc629f632b2bea62b884044
Walid Cheikh <walid.cheikh@flatironssolutions.com>
2019-09-10 17:33:21 +0200

    Added default access modifier

--- src/dk/cngroup/training/java8/labmda/TaxLaw.java
+++ src/dk/cngroup/training/java8/labmda/TaxLaw.java
@@ -2,5 +2,5 @@ package dk.cngroup.training.java8.labmda;
 
 public interface TaxLaw {
 	double calculateTaxes(double income);
-	//int calculateTaxesV2(double income1, double income2){return 0;}
+	//default int calculateTaxesV2(double income1, double income2){return 0;}
 }
